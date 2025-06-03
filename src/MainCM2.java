5773ae79186befeb1f5337cacec44b3e357a8c7f
Badiatul-06 <badiatulmala@gmail.com>
2025-06-03 09:05:54 +0700

    Add Main

---
+++ src/MainCM2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class MainCM2 {
+    public static void main(String[] args) throws Exception{
+        Scanner input = new Scanner(System.in);
+
+        int pilihan;
+
+        do {
+            System.out.println("\n++++++++++++++++++++++++++++++++");
+            System.out.println("Sistem Antrian SPBU");
+            System.out.println("++++++++++++++++++++++++++++++++");
+            System.out.println("1. Tambah Antrian");
+            System.out.println("2. Lihat Antrian");
+            System.out.println("3. Cek Sisa Antrian Kendaraan");
+            System.out.println("4. Layani Kendaraan");
+            System.out.println("5. Lihat Riwayat Transaksi");
+            System.out.println("6. Sort Riwayat Transaksi");
+            System.out.println("0. Keluar");
+            System.out.println("++++++++++++++++++++++++++++++++");
+            System.out.print("Pilih menu : ");
+            pilihan = input.nextInt();
+            input.nextLine();
+            switch (pilihan) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 0:
+                    System.out.println("\nTerima Kasih!"); 
+                    break;           
+                default:
+                    System.out.println("\nPilihan tidak valid.");
+            }
+        } while (pilihan != 0);
+
+        input.close();
+    }
+}
